e99715dc4efa04889094656a3ad51cb4c5032872
Lautaro Desouches <lautarodesouches@outlook.com>
2022-06-03 18:11:13 -0300

    [App] FlatList

--- App.js
+++ App.js
@@ -11,20 +11,35 @@ export default function App() {
     // Clear task
     setTask('');
     // Add task to list
-    setTaskList([...taskList, task]);
+    setTaskList([...taskList, { id: Math.random(), value: task }]);
+  }
+
+  const ListHeaderComponent = () => {
+    return <Text style={styles.list__title}>Tareas:</Text>
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <View styke={styles.list__item} key={item.id}>
+        <Text styke={styles.list__text}>{item.value}</Text>
+      </View>
+    );
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.list}>
-        {
-          taskList.map( (el, id) => <Text key={id}>{el}</Text>)
-        }
+      <View style={styles.listContainer}>
+        <FlatList
+          data={taskList}
+          renderItem={renderItem}
+          ListHeaderComponent={ListHeaderComponent}
+          style={styles.list}
+        />
       </View>
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder="Escribir tarea" value={task} onChangeText={e => setTask(e)} />
         <TouchableOpacity style={styles.buttonPrimary} onPress={() => handleSumbit()}>
-          <Text style={styles.buttonPrimary__Text}>Agregar</Text>
+          <Text style={styles.buttonPrimary__text}>Agregar</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -53,9 +68,20 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: 20,
   },
-  buttonPrimary__Text: {
+  buttonPrimary__text: {
     color: primaryText,
     fontSize: 17,
     textAlign: 'center',
+  },
+  list__title: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  list__item: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  list__text: {
+    fontSize: 15,
   }
 });
